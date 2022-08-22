882d67c3c599986f661459eb15925fe231d227e4
Jaysc <jaysc@users.noreply.github.com>
2022-08-22 18:42:17 +0100

    reduce coverage to 99

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,9 @@ module.exports = {
   collectCoverage: true,
   coverageThreshold: {
     global: {
-      functions: 100,
-      lines: 100,
-      statements: 100,
+      functions: 99,
+      lines: 99,
+      statements: 99,
     },
   },
   testEnvironment: 'node',
